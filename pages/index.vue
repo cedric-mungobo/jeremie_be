4e5c367fb9a050de4362ad2a0320a073542d6f96
Cedric Mungobo <sedrickmungobo1Gmail.com>
2024-08-24 09:00:55 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,102 @@
     class="ezy__header30 light relative overflow-hidden text-white bg-white z-10"
   >
     <div
-      class="ezy__header30-topbar py-36 md:py-56 bg-center bg-no-repeat bg-cover relative flex items-center justify-center z-[1]"
+      class="ezy__header30-topbar py-36 md:py-[18rem] max-sm:bg-center bg-no-repeat bg-cover relative flex items-center justify-center z-[1]"
       style="background-image: url(/assets/Couverture.jpg)"
     >
       <div
         class="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-60 -z-10"
       ></div>
       <div class="container px-4 mx-auto">
+        <header
+          class="flex items-center justify-center px-10 top-0 absolute w-full mx-auto py-2"
+        >
+          <nav class="flex flex-1 max-w-5xl justify-between items-center">
+            <a
+              class="flex-none font-semibold text-xl text-gray-100 focus:outline-none focus:opacity-80"
+              href="#"
+              aria-label=""
+              >Jeremie Be</a
+            >
+            <ul class="flex items-center gap-3">
+              <li>
+                <button
+                  class="md:w-10 md:h-10 flex items-center justify-center relative overflow-hidden rounded-full bg-white shadow-md shadow-gray-200 group transition-all duration-300"
+                >
+                  <svg
+                    class="relative z-10 fill-gray-900 transition-all duration-300 group-hover:fill-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="28"
+                    height="28"
+                    viewBox="0 0 72 72"
+                    fill="none"
+                  >
+                    <path
+                      d="M46.4927 38.6403L47.7973 30.3588H39.7611V24.9759C39.7611 22.7114 40.883 20.4987 44.4706 20.4987H48.1756V13.4465C46.018 13.1028 43.8378 12.9168 41.6527 12.8901C35.0385 12.8901 30.7204 16.8626 30.7204 24.0442V30.3588H23.3887V38.6403H30.7204V58.671H39.7611V38.6403H46.4927Z"
+                      fill=""
+                    />
+                  </svg>
+                  <div
+                    class="absolute top-full left-0 w-full h-full rounded-full bg-blue-500 z-0 transition-all duration-500 group-hover:top-0"
+                  ></div>
+                </button>
+              </li>
+              <li>
+                <button
+                  class="md:w-10 md:h-10 flex items-center relative overflow-hidden justify-center rounded-full bg-white shadow-md shadow-gray-200 group transition-all duration-300"
+                >
+                  <svg
+                    class="fill-gray-900 relative z-10 transition-all duration-300 group-hover:fill-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="28"
+                    height="28"
+                    viewBox="0 0 72 72"
+                    fill="none"
+                  >
+                    <path
+                      d="M24.7612 55.999V28.3354H15.5433V55.999H24.7621H24.7612ZM20.1542 24.5591C23.3679 24.5591 25.3687 22.4348 25.3687 19.7801C25.3086 17.065 23.3679 15 20.2153 15C17.0605 15 15 17.065 15 19.7799C15 22.4346 17.0001 24.5588 20.0938 24.5588H20.1534L20.1542 24.5591ZM29.8633 55.999H39.0805V40.5521C39.0805 39.7264 39.1406 38.8985 39.3841 38.3088C40.0502 36.6562 41.5668 34.9455 44.1138 34.9455C47.4484 34.9455 48.7831 37.4821 48.7831 41.2014V55.999H58V40.1376C58 31.6408 53.4532 27.6869 47.3887 27.6869C42.4167 27.6869 40.233 30.4589 39.0198 32.347H39.0812V28.3364H29.8638C29.9841 30.9316 29.8631 56 29.8631 56L29.8633 55.999Z"
+                      fill=""
+                    />
+                  </svg>
+                  <div
+                    class="absolute top-full left-0 w-full h-full rounded-full bg-[#006699] z-0 transition-all duration-500 group-hover:top-0"
+                  ></div>
+                </button>
+              </li>
+              <li>
+                <button
+                  class="md:w-10 md:h-10 flex items-center relative overflow-hidden justify-center rounded-full bg-white shadow-md shadow-gray-200 group transition-all duration-300"
+                >
+                  <svg
+                    class="fill-gray-900 relative z-10 transition-all duration-300 group-hover:fill-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 72 72"
+                    fill="none"
+                  >
+                    <path
+                      d="M61.1026 23.7185C60.5048 21.471 58.7363 19.6981 56.4863 19.0904C52.4181 18 36.0951 18 36.0951 18C36.0951 18 19.7805 18 15.7039 19.0904C13.4622 19.6897 11.6937 21.4627 11.0876 23.7185C10 27.7971 10 36.3124 10 36.3124C10 36.3124 10 44.8276 11.0876 48.9063C11.6854 51.1537 13.4539 52.9267 15.7039 53.5343C19.7805 54.6247 36.0951 54.6247 36.0951 54.6247C36.0951 54.6247 52.4181 54.6247 56.4863 53.5343C58.728 52.935 60.4965 51.162 61.1026 48.9063C62.1902 44.8276 62.1902 36.3124 62.1902 36.3124C62.1902 36.3124 62.1902 27.7971 61.1026 23.7185Z"
+                      fill=""
+                    />
+                    <path
+                      class="fill-white transition-all duration-300 group-hover:fill-[#FF3000]"
+                      d="M30.8811 44.1617L44.4392 36.3124L30.8811 28.463V44.1617Z"
+                      fill="white"
+                    />
+                  </svg>
+                  <div
+                    class="absolute top-full left-0 w-full h-full rounded-full bg-[#FF3000] z-0 transition-all duration-500 group-hover:top-0"
+                  ></div>
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
         <div class="max-w-5xl flex justify-center text-center mx-auto">
           <div>
-            <h1 class="text-3xl font-bold leading-tight md:text-6xl mb-6">
+            <h1 class="text-3xl font-bold leading-tight md:text-6xl mb-10">
               Lancez-vous sur Internet avec confiance, que vous soyez amateur ou
               professionnel !
             </h1>
@@ -29,11 +115,11 @@
       </div>
     </div>
     <!-- courses carousel -->
-    <div class="container px-4 mx-auto mb-2 -mt-36 md:-mt-48">
+    <div class="container px-4 mx-auto mb-2 py-10 max-w-5xl">
       <div class="z-10 relative mt-1">
-        <section class="py-2 text-gray-100">
-          <div class="container md:px-4 px-2 mx-auto">
-            <div class="max-w-4xl mx-auto mb-6 text-center">
+        <section class="py-2 text-gray-800">
+          <div class="container md:px-2 px-2 mx-auto">
+            <div class="max-w-4xl mx-auto mb-5 text-center">
               <h2 class="text-2xl font-bold md:text-5xl">
                 Choisissez le forfait de formation qui vous convient le mieux !
               </h2>
@@ -41,19 +127,15 @@
             <div class="flex flex-wrap items-stretch -mx-4">
               <div class="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/2 lg:mb-0">
                 <div
-                  class="flex flex-grow flex-col p-6 space-y-6 rounded-xl shadow-md sm:p-8 bg-gray-400 text-black"
+                  class="flex flex-grow flex-col p-2 space-y-2 rounded-xl shadow-md sm:p-8 bg-gradient-to-r from-black to-slate-800 text-gray-100"
                 >
                   <div class="space-y-1">
                     <h4 class="text-2xl font-bold">Forfait</h4>
                     <span class="text-4xl font-bold">Découverte</span>
                   </div>
-                  <p class="mt-0 leading-relaxed text-gray-800">
-                    {{ package1.time }}
-                  </p>
-                  <span class="p-0"
-                    >Début de la séance : Ce 01 septembre 2024
-                  </span>
-                  <ul class="flex-1 mb-6 text-gray-800">
+
+                  <span class="p-0">Modules </span>
+                  <ul class="flex-1 mb-6 text-gray-100">
                     <li
                       class="flex mb-2 space-x-2"
                       v-for="(item, i) in package1.contenu"
@@ -63,7 +145,7 @@
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 20 20"
                         fill="currentColor"
-                        class="flex-shrink-0 w-6 h-6 text-gray-800"
+                        class="flex-shrink-0 w-6 h-6 text-gray-100"
                       >
                         <path
                           fill-rule="evenodd"
@@ -84,7 +166,7 @@
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 20 20"
                         fill="currentColor"
-                        class="flex-shrink-0 w-6 h-6 text-gray-800"
+                        class="flex-shrink-0 w-6 h-6 text-gray-100"
                       >
                         <path
                           fill-rule="evenodd"
@@ -95,26 +177,32 @@
                       <span>{{ item }}</span>
                     </li>
                   </ul>
+
+                  <span class="p-0"
+                    >Début de la séance : Ce 01 septembre 2024
+                  </span>
+                  <p class="mt-0 leading-relaxed text-gray-100">
+                    {{ package1.time }}
+                  </p>
                   <button
                     type="button"
-                    class="inline-block px-5 py-3 font-semibold tracking-wider text-center rounded-lg bg-gradient-to-r from-black to-slate-800 text-gray-100"
+                    class="inline-block px-5 py-3 font-semibold tracking-wider text-center rounded-lg bg-white text-gray-900"
                   >
+                    <span class="text-xl pr-2 font-bold">150$</span>
                     Optez pour votre formation
                   </button>
                 </div>
               </div>
               <div class="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/2 lg:mb-0">
                 <div
-                  class="flex flex-grow flex-col p-6 space-y-6 rounded-xl shadow sm:p-8 bg-gradient-to-r from-black to-slate-800 text-gray-100"
+                  class="flex flex-grow flex-col p-2 space-y-2 rounded-xl shadow sm:p-8 bg-gradient-to-r from-black to-slate-800 text-gray-100"
                 >
                   <div class="space-y-2">
                     <h4 class="text-2xl font-bold">Forfait</h4>
                     <span class="text-4xl font-bold">Intermédiaire </span>
                   </div>
-                  <p class="leading-relaxed">
-                    {{ package2.time }}
-                  </p>
-                  <p class="p-0">Début de la séance : Ce 01 septembre 2024</p>
+
+                  <p class="p-0">Modules</p>
                   <ul class="flex-1 space-y-2">
                     <li
                       class="flex mb-2 space-x-2"
@@ -158,11 +246,18 @@
                       <span>{{ item }}</span>
                     </li>
                   </ul>
+
+                  <p class="leading-relaxed">
+                    {{ package2.time }}
+                  </p>
+                  <p class="p-0">Début de la séance : Ce 01 septembre 2024</p>
                   <a
                     rel="noopener noreferrer"
                     href="#"
-                    class="inline-block w-full px-5 py-3 font-bold tracking-wider text-center rounded-lg bg-gray-300 text-gray-900"
-                    >Optez pour votre formation
+                    class="inline-block w-full px-5 py-3 font-bold tracking-wider text-center rounded-lg bg-white text-gray-900"
+                  >
+                    <span class="text-xl pr-2 font-bold">250$</span>
+                    Optez pour votre formation
                   </a>
                 </div>
               </div>
@@ -246,6 +341,14 @@
               />
             </div>
           </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="pb-16">
+      <div class="flex justify-center text-red-600">
+        <div class="max-w-6xl">
+          <img src="/assets/entreprise.png" alt="" />
         </div>
       </div>
     </section>
